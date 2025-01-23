ac6fdce8e8b8a9fec956dae241b49b4066bbd94c
Samuel Gbafa <skgbafa@gmail.com>
2025-01-23 13:49:18 +0000

    cleanup

---
+++ scripts/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clean each package
+packages=(
+  "packages/web-sdk"
+  "packages/web-core"
+  "packages/web-sdk-rs"
+  "examples/react-test-app"
+)
+
+for package in "${packages[@]}"; do
+  echo "Cleaning $package..."
+  rm -rf "$package/node_modules"
+  rm -rf "$package/dist"
+  rm -rf "$package/temp"
+done
+
+# Clean root node_modules
+echo "Cleaning root node_modules..."
+rm -rf node_modules
